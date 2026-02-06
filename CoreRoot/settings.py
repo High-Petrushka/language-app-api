82147641f68047d4bdd83523f4b2626565a297ce
High-Petrushka <daniilprokopovic12@gmail.com>
2026-02-06 06:40:28 +0300

    Adding a media settings

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -103,6 +104,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # DRF Settings
 REST_FRAMEWORK = {
